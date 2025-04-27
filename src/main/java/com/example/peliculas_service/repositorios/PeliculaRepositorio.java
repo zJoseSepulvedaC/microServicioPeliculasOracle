478d4eb379cf9b5a5bbfeb8a516da3bd6ab1b9c6
zJoseSepulvedaC <zabatgunz2@gmail.com>
2025-04-27 17:15:43 -0400

    Cambios

--- src/main/java/com/example/peliculas_service/repositorios/PeliculaRepositorio.java
+++ src/main/java/com/example/peliculas_service/repositorios/PeliculaRepositorio.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.peliculas_service.repositorios;
 
 import com.example.peliculas_service.model.Pelicula;
@@ -8,14 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PeliculaRepositorio extends JpaRepository<Pelicula, Long> {
 }
-=======
-package com.example.peliculas_service.repositorios;
-
-import com.example.peliculas_service.model.Pelicula;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeliculaRepositorio extends JpaRepository<Pelicula, Long> {
-}
->>>>>>> ecd67f2f6b10dade2fee15f606f71ceb6341b664
